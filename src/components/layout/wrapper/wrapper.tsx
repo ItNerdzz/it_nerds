512639b88f57f80067aeea2145c215390f7572c0
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-01-24 20:48:20 +0400

    wrapper filename, className update

--- src/components/layout/wrapper/wrapper.tsx
+++ src/components/layout/wrapper/wrapper.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
-import styles from './Wrapper.module.css';
+import styles from './wrapper.module.css';
 
 interface IWrapperProps {
   children: React.ReactNode;
 }
 
 const Wrapper: FC<IWrapperProps> = ({ children }) => {
-  return <div className={styles.wrapper}>{children}</div>;
+  return <div className={styles.Wrapper}>{children}</div>;
 };
 
 export default Wrapper;
