6ff61ba581a6b8d0e38c92bcdfa1cfb936f79d17
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-12-23 23:43:54 +0400

    Services text update

--- src/components/home/Services/Services.tsx
+++ src/components/home/Services/Services.tsx
@@ -5,36 +5,91 @@ import { Title } from '@/components/ui';
 import { Tabs } from '@/components/common';
 import { ServiceItem } from '@/components/home';
 
-import { IServicesData, IServicesProps, IGeneratedTabItems } from './interface';
 import styles from './Services.module.css';
 
-const generateTabItems = (data: IServicesData[]) => {
-  const newData: IGeneratedTabItems[] = [];
-
-  data.forEach(item => {
-    newData.push({
-      tabTitle: item.category,
+const Services: FC = () => {
+  const servicesData = [
+    {
+      tabTitle: 'Разработка',
       content: (
         <ul className={styles.list}>
-          {item.items.map(item => (
-            <ServiceItem
-              key={item.id}
-              serviceItem={{
-                title: item.title,
-                description: item.description,
-                price: item.price,
-              }}
-            />
+          {[
+            {
+              title: 'Визитка',
+              description:
+                'Мини-сайт для быстрого представления компании. Включает информацию о деятельности, контактные данные и список услуг.',
+              price: 15000,
+            },
+            {
+              title: 'Лендинг',
+              description:
+                'Посадочная страница для сбора контактов клиентов, регистрации или покупок. Является эффективным инструментом презентации, раскрывая преимущества продукта и создавая доверие между посетителем и компанией.',
+              price: 45000,
+            },
+            {
+              title: 'Многостраничный сайт',
+              description:
+                'Информационный сайт, который представляет компанию в интернете. Идеален для размещения полной информации о компании: описание услуг, преимущества, новости, сотрудничество, продукция и контактные данные.',
+              price: 85000,
+            },
+            {
+              title: 'Интернет магазин',
+              description:
+                'Онлайн-платформа для продажи товаров или услуг. Включает карточки товаров, категории, фильтрацию, онлайн-оплату, систему управления заказами и дополнительные интеграции. Управление ценами, скидками, и возможностью импорта/экспорта продукции, а также наличие онлайн-консультанта и дополнительных виджетов.',
+              price: 120000,
+            },
+          ].map(item => (
+            <ServiceItem key={item.title} serviceItem={item} />
           ))}
         </ul>
       ),
-    });
-  });
+    },
+    {
+      tabTitle: 'Продвижение',
+      content: (
+        <ul className={styles.list}>
+          {[
+            {
+              title: 'SEO продвижение',
+              description:
+                'Комплексный анализ вашего сайта и конкурентов. Определяем точки роста и реализуем все технические задачи нашей командой.',
+              price: 15000,
+            },
+            {
+              title: 'Таргет',
+              description: 'Настройка рекламы бесплатно — вы платите только за ведение рекламной кампании.',
+              price: 15000,
+            },
+          ].map(item => (
+            <ServiceItem key={item.title} serviceItem={item} />
+          ))}
+        </ul>
+      ),
+    },
+    {
+      tabTitle: 'Другие услуги',
+      content: (
+        <ul className={styles.list}>
+          {[
+            {
+              title: 'Чат-бот для мессенджера',
+              description: 'Создание чат-бота под ваши задачи и цели.',
+              price: 10000,
+            },
+            {
+              title: 'Оформление GetCourse',
+              description:
+                'Разработка современного дизайна для ключевых страниц и блоков вашей образовательной платформы.',
+              price: 25000,
+            },
+          ].map(item => (
+            <ServiceItem key={item.title} serviceItem={item} />
+          ))}
+        </ul>
+      ),
+    },
+  ];
 
-  return newData;
-};
-
-const Services: FC<IServicesProps> = ({ servicesData }) => {
   return (
     <section id={'Services'}>
       <Wrapper>
@@ -42,7 +97,7 @@ const Services: FC<IServicesProps> = ({ servicesData }) => {
           <Title className={styles.title} size={'medium'} as={'h2'}>
             Наши услуги
           </Title>
-          <Tabs tabItems={generateTabItems(servicesData)} />
+          <Tabs tabItems={servicesData} />
         </div>
       </Wrapper>
     </section>
